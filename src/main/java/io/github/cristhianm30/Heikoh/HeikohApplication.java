05001a15b337460d3858233d0e6c9775f63db989
Cristhian Andres Moreno Velasco <Cristhian.452@outlook.com>
2025-06-27 19:22:43 -0500

    initial commit

---
+++ src/main/java/io/github/cristhianm30/Heikoh/HeikohApplication.java
@@ -0,0 +1,13 @@
+package io.github.cristhianm30.Heikoh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeikohApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeikohApplication.class, args);
+	}
+
+}
